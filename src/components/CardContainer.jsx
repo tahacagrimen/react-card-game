e5dae1f100c56ec652ce6be145037c236c1b7f7a
tahacagrimen <tahacagrimen2@gmail.com>
2022-08-23 18:29:57 +0300

    card game is over

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -3,12 +3,31 @@ import data from "../data";
 import Card from "./Card";
 
 function CardContainer() {
+  const [gameOver, setGameOver] = useState(false);
+  //
+  //
+  // STOPWATCH
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+  // STOPWATCH
+  //
+  //
+
   const [cards, setCards] = useState([]);
 
-  // Set timer
-  const [timer, setTimer] = useState(0);
-
   const shuffleCards = () => {
+    setGameOver(false);
     const shuffledCards = [...data, ...data]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({
@@ -16,7 +35,8 @@ function CardContainer() {
         id: Math.random(),
       }));
     setCards(shuffledCards);
-    setTimer(0);
+    setRunning(false);
+    setTime(0);
   };
 
   // Setting up choices
@@ -27,10 +47,12 @@ function CardContainer() {
     if (!choiceTwo) {
       choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
     }
+    setRunning(true);
   };
 
   // compare
   useEffect(() => {
+    setGameOver(false);
     if (choiceOne && choiceTwo) {
       if (choiceOne.cardName === choiceTwo.cardName) {
         setCards((prev) => {
@@ -44,12 +66,18 @@ function CardContainer() {
         });
         reset();
       } else {
-        setTimeout(() => reset(), 1000);
+        setTimeout(() => reset(), 500);
       }
+    }
+    if (cards.every((card) => card.matched) && cards.length > 0) {
+      setGameOver(true);
+      setRunning(false);
     }
   }, [choiceOne, choiceTwo]);
 
   // On page load shuffle cards
+  //
+  //
   const [shouldRender, setShouldRender] = useState(true);
 
   useEffect(() => {
@@ -58,13 +86,11 @@ function CardContainer() {
 
   // don't render
   if (!shouldRender) return null;
+  //
+  //
+  // On page load shuffle cards
 
-  // if every card is open end the game and show the score
-  const matchedCards = cards.length
-    ? cards.every((card) => card.matched)
-    : false;
-  console.log(matchedCards);
-
+  // Reset choiced cards
   const reset = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
@@ -72,27 +98,43 @@ function CardContainer() {
 
   return (
     <div>
-      <div className="flex items-center justify-between p-2">
+      <div className="flex items-center justify-between">
         <button
           onClick={shuffleCards}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 mr-4"
         >
-          Shuffle Cards
+          {gameOver ? "Play Again" : "Shuffle Cards"}
         </button>
-        <h2>Time</h2>
+        <div className="numbers">
+          <span className="font-bold text-lg">
+            {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+          </span>
+          <span className="font-bold text-lg">
+            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}:
+          </span>
+          <span className="font-bold text-lg">
+            {("0" + ((time / 10) % 100)).slice(-2)}
+          </span>
+        </div>
       </div>
-      <div className="card-container grid grid-cols-4 grid-rows-4 gap-4">
-        {cards.map((card) => (
-          <Card
-            key={card.id}
-            card={card}
-            handleChoice={handleChoice}
-            choiceOne={choiceOne}
-            choiceTwo={choiceTwo}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-          />
-        ))}
-      </div>
+      {gameOver ? (
+        <div className="text-center text-2xl font-bold w-full h-full">
+          <span className="text-red-500">You Win!</span>
+        </div>
+      ) : (
+        <div className="card-container grid grid-cols-4 grid-rows-4 gap-4">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              card={card}
+              handleChoice={handleChoice}
+              choiceOne={choiceOne}
+              choiceTwo={choiceTwo}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
